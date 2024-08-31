c53d4a6f6b2179ae08819650f1aede3f52f9bd09
suraj maurya <mnaveen1888@gmail.com>
2024-08-31 17:12:59 +0530

    add node-red-dashboard

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ RED.init(server, settings);
 
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Node-RED is running on port 3000");
