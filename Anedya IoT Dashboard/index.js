b6936137c1b0cbb12a7fadfb26ee566ebddebfc0
suraj maurya <mnaveen1888@gmail.com>
2024-10-17 16:48:25 +0530

    remove port opening intergration

--- Anedya IoT Dashboard/index.js
+++ Anedya IoT Dashboard/index.js
@@ -28,21 +28,11 @@ app.use((req, res, next) => {
     next();
 });
 
-// Uncomment if you are deploying the flow in production.
+
 server.listen(process.env.PORT || 3000, () => {
     console.log("Node-RED is running on port 3000");
 });
 
-//Uncomment if you are running locally
-// server.listen(process.env.PORT || 3000, async () => {
-//     console.log("Node-RED is running on port 3000");
-//     const open = (await import('open')).default; // Dynamic import
-    
-//     // Set a delay of 2 seconds before opening the URL
-//     setTimeout(() => {
-//         open(`http://localhost:3000`);
-//     }, 2000); // 2000 milliseconds = 2 seconds
-// });
 
 
 RED.start();
